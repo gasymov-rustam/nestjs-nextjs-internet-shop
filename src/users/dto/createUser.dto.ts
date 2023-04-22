7b5ba2948bc78fe0687ebb0a0c3ee93e5f200aae
Rustam Gasymov <gasymov86@gmail.com>
2023-04-22 11:48:56 +0300

    add dto and validator

---
+++ src/users/dto/createUser.dto.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+}
