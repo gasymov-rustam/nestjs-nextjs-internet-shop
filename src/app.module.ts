f542d88828fd6968dd09a5e9093114e97d8a3247
Rustam Gasymov <gasymov86@gmail.com>
2023-04-22 11:41:59 +0300

    add docker compose and all configs for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { dataBaseConfig } from './config/configuration';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({
+      load: [dataBaseConfig],
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
