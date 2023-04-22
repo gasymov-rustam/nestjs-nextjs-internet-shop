1ca2f0cba25fa83e9631bdadece9c9cb409c14f4
Rustam Gasymov <gasymov86@gmail.com>
2023-04-22 12:17:47 +0300

    add users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-Type', 'application/json')
+  createUser(@Body() user: CreateUserDto) {
+    return this.usersService.create(user);
+  }
+}
